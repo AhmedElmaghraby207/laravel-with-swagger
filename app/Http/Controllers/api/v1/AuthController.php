748411022d3f5b5faa3bcbce427f3a2e88361c1b
ahmed_elmaghraby <aerfan@softmills.com>
2021-06-09 18:10:44 +0200

    add swagger api documentaion

---
+++ app/Http/Controllers/api/v1/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+class AuthController extends BaseApiController
+{
+    /**
+     * @OA\Post(
+     * path="/login",
+     * summary="Sign in",
+     * description="Login by email, password",
+     * operationId="authLogin",
+     * tags={"Auth"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Pass user credentials",
+     *    @OA\JsonContent(
+     *       required={"email","password"},
+     *       @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+     *       @OA\Property(property="password", type="string", format="password", example="PassWord12345"),
+     *       @OA\Property(property="persistent", type="boolean", example="true"),
+     *    ),
+     * ),
+     * @OA\Response(
+     *    response=422,
+     *    description="Wrong credentials response",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
+     *        )
+     *     )
+     * )
+     */
+    public function login()
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/api/v1/ProjectsController.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use Illuminate\Http\Request;
+
+class ProjectsController extends BaseApiController
+{
+    /**
+     * @OA\Get(
+     *    path="/projects",
+     *    summary="List projects",
+     *    description="List all projects",
+     *    operationId="projectsList",
+     *    tags={"Projects"},
+     *    @OA\Response(
+     *        response=200,
+     *        description="Retun projects list",
+     *        @OA\JsonContent(@OA\Property(property="data", type="string", example="[]"))
+     *     ),
+     *    @OA\Response(
+     *        response=401,
+     *        description="Unauthenticated",
+     *    ),
+     *    @OA\Response(
+     *        response=403,
+     *        description="Forbidden"
+     *    )
+     * )
+     */
+    public function list()
+    {
+        //
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/projects",
+     *      operationId="storeProject",
+     *      tags={"Projects"},
+     *      summary="Store new project",
+     *      description="Returns project data",
+     *      @OA\RequestBody(
+     *          required=true,
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *          @OA\JsonContent(@OA\Property(property="message", type="string", example="project created successfully"))
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/projects/{id}",
+     *      operationId="getProjectById",
+     *      tags={"Projects"},
+     *      summary="Get project information",
+     *      description="Returns project data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(@OA\Property(property="data", type="string", example="project data"))
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/projects/{id}",
+     *      operationId="updateProject",
+     *      tags={"Projects"},
+     *      summary="Update existing project",
+     *      description="Returns updated project data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *      ),
+     *      @OA\Response(
+     *          response=202,
+     *          description="Successful operation",
+     *          @OA\JsonContent(@OA\Property(property="message", type="string", example="project updated successfully"))
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
+     */
+    public function update(Request $request, $id)
+    {
+    }
+
+    /**
+     * @OA\Delete(
+     *      path="/projects/{id}",
+     *      operationId="deleteProject",
+     *      tags={"Projects"},
+     *      summary="Delete existing project",
+     *      description="Deletes a record and returns no content",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=204,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
+     */
+    public function destroy($id)
+    {
+    }
+}
